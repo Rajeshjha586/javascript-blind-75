3ae915f71ee63801dfa1948e6dfcd82cbf103a74
Rajesh Jha <jharajesh035@gmail.com>
2025-07-26 23:00:45 +0530

    feat: Problem 21 binary tree serialize and deserialize

---
+++ 21_binaryTreeSerialize&Deserialize.js
@@ -0,0 +1,66 @@
+/**
+ * @param {TreeNode | null} root
+ * @return {string}
+ */
+export function serializeBinaryTree(root) {
+  if (!root) {
+    return "";
+  }
+
+  let result = "";
+  const queue = [root];
+
+  while (queue.length) {
+    let current = queue.shift();
+
+    if (current === null) {
+      result += "#,";
+    } else {
+      result += current.val + ",";
+      queue.push(current.left);
+      queue.push(current.right);
+    }
+  }
+
+  return result;
+}
+
+/**
+ * @param {string} data
+ * @return {TreeNode | null}
+ */
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export function deserializeBinaryTree(data) {
+  if (data === "#") return null;
+  const arr = data.split(",");
+  let root = new TreeNode(parseInt(arr[0]));
+  let queue = [root];
+  let index = 1;
+
+  while (queue.length) {
+    let node = queue.shift();
+    if (arr[index] !== "#") {
+      node.left = new TreeNode(parseInt(arr[index]));
+      queue.push(node.left);
+    }
+
+    index++;
+
+    if (arr[index] !== "#") {
+      node.right = new TreeNode(parseInt(arr[index]));
+      queue.push(node.right);
+    }
+
+    index++;
+  }
+
+  return root;
+}
